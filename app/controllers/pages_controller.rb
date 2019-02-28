c9659c55093c9a85b6b2dd54037ef2b0c5f4f053
Михман <miklblitz@yandex.ru>
2019-02-28 14:53:50 +0400

    main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
 
+  def index
+    @data = [
+      title: "API for fondy.eu",
+      description: "Testing the Payment Gate for fondy.eu",
+      version: "0.1.0"
+    ]
+    render json: @data
+  end
+
   def success
     render 'success'
   end
